cb9d3b4dc09eeb3e50f94d912f37198195a07972
Samo <s.cajnar@sportradar.com>
2024-10-04 10:17:46 +0200

    chore: cleanup unused imports

--- test_example.py
+++ test_example.py
@@ -1,17 +1,19 @@
 import re
+import time
+
 from playwright.sync_api import Page, expect
 
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
-
+    time.sleep(3)
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
-
+    time.sleep(3)
     # Click the get started link.
     page.get_by_role("link", name="Get started").click()
 

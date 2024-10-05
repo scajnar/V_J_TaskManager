01b875df0dae690f1eda5f676045fb4370ecbc37
Samo <s.cajnar@sportradar.com>
2024-10-05 14:36:33 +0200

    refactor: remove TaskList element, since it is useless. It only represented <ul>

--- test_login.py
+++ test_login.py
@@ -1,14 +1,16 @@
-import inspect
 import logging
 import time
 from enum import Enum
-from functools import wraps
 
 import pytest
-from playwright.sync_api import expect, Playwright
-from playwright.sync_api._generated import Locator, Page
+from playwright.sync_api import expect, Page
+from playwright.sync_api._generated import Locator
 
 logging.basicConfig(level=logging.INFO)
+
+
+def get_current_page(page):
+    return page.locator("//body")
 
 
 # Defining the text that will be used in the tests to avoid typos.
@@ -27,71 +29,98 @@ class Text(Enum):
     SHOW_COMPLETED_TASKS = "Show Completed Tasks"
 
 
-def get_current_page(page):
-    return page.locator("//body")
-
-
 class BaseElement:
     def __init__(self, locator: Locator):
         self.locator = locator
         assert self.locator, "The element was not found."
 
+    def __call__(self):
+        return self.locator
+
     def _locator(self, xpath: str, *args, **kwargs):
         return self.locator.locator(f"xpath={xpath}", *args, **kwargs)
 
     def locate(self, xpath: str, wait: int = 0, *args, **kwargs):
-        time_start = time.time()
-        found = False
-        if not wait:
-            return self._locator(xpath, *args, **kwargs)
-        while not found and (time.time() - time_start < wait):
-            if self._locator(xpath, *args, **kwargs):
-                return self._locator(xpath, *args, **kwargs)
-            time.sleep(0.1)  # To avoid CPU hogging
-        else:
-            raise TimeoutError(
-                f"Element with xpath: {xpath}\n not found withing timeframe {wait}s "
-            )
+        target_locator = self._locator(xpath, *args, **kwargs)
+        if wait:
+            try:
+                target_locator.wait_for(state="attached", timeout=wait * 1000)
+            except Exception:
+                raise TimeoutError(
+                    f"Element with xpath: {xpath}\n not found within timeframe {wait}s"
+                )
+        return target_locator
+
+
+class TaskWithCheckboxAndButton(BaseElement):
+    def __init__(self, locator: Locator):
+        super().__init__(locator)
+        self.task_text_xpath: str = ".//span"
+        self.checkbox_xpath: str = ".//input"
+        self.button_xpath: str = ".//button"
+
+    @property
+    def text_elem(self):
+        return self.locate(self.task_text_xpath)
+
+    @property
+    def text(self):
+        return self.text_elem.inner_text()
+
+    @property
+    def checkbox(self):
+        return self.locate(self.checkbox_xpath)
+
+    @property
+    def button(self):
+        return self.locate(self.button_xpath)
+
+    @property
+    def is_text_line_through(self):
+        style = self.text_elem.get_attribute("style")
+        return "line-through" in style
+
+
+class TaskWithTextOnly(BaseElement):
+    def __init__(self, locator: Locator):
+        super().__init__(locator)
+
+    @property
+    def text(self):
+        return self.locator.inner_text()
 
 
 class Card(BaseElement):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, locator: Locator):
+        super().__init__(locator)
 
     # We'll be able to access all title elements with this property.
     @property
     def title(self):
-        return self.get_attribute(".//h2")
+        return self.locate(".//h2").inner_text()
 
 
 class TaskManagerPage(BaseElement):
-    def __init__(self, body: Locator):
-        super().__init__(body)
-        self.daily_tip_card_text = Text.DAILY_TIP.value
-        self.add_new_task_card_text = Text.ADD_NEW_TASK.value
-        self.task_list_card_text = Text.TASK_LIST.value
-        self.completed_tasks_card_text = Text.COMPLETED_TASKS.value
-
+    def __init__(self, locator: Locator):
+        super().__init__(locator)
         self.card_base_xpath = ".//div/*[contains(text(), '{text}')]/parent::div"
 
         self.daily_tip_card = TaskManagerPage.DailyTipCard(
-            self.locate(self.card_base_xpath.format(text=self.daily_tip_card_text))
+            self.locate(self.card_base_xpath.format(text=Text.DAILY_TIP.value))
         )
         self.add_new_task_card = TaskManagerPage.AddNewTaskCard(
-            self.locate(self.card_base_xpath.format(text=self.add_new_task_card_text))
+            self.locate(self.card_base_xpath.format(text=Text.ADD_NEW_TASK.value))
         )
         self.task_list_card = TaskManagerPage.TaskListCard(
-            self.locate(self.card_base_xpath.format(text=self.task_list_card_text))
+            self.locate(self.card_base_xpath.format(text=Text.TASK_LIST.value))
         )
         self.completed_tasks_card = TaskManagerPage.CompletedTasks(
-            self.locate(
-                self.card_base_xpath.format(text=self.completed_tasks_card_text)
-            )
+            self.locate(self.card_base_xpath.format(text=Text.COMPLETED_TASKS.value))
         )
 
     class DailyTipCard(Card):
-        def __init__(self, body: Locator):
-            super().__init__(body)
+        def __init__(self, locator: Locator):
+            super().__init__(locator)
             self.tip_text_xpath = ".//p"
 
         # We do not want to initialize the tip_text_elem until we need it.
@@ -100,81 +129,48 @@ class TaskManagerPage(BaseElement):
             return self.locate(self.tip_text_xpath)
 
     class AddNewTaskCard(Card):
-        def __init__(self, body: Locator):
-            super().__init__(body)
+        def __init__(self, locator: Locator):
+            super().__init__(locator)
             self.input_field_xpath = ".//input"
             self.button_xpath = ".//button"
             self.button = self.locate(self.button_xpath)
             self.input_field = self.locate(self.input_field_xpath)
 
     class TaskListCard(Card):
-        def __init__(self, body: Locator):
-            super().__init__(body)
-            self.task_list_xpath: str = ".//ul"
-            self.task_list = ...
+        def __init__(self, locator: Locator):
+            super().__init__(locator)
+            self.tasks_xpath: str = ".//li"
+            self.tasks = []
 
-        def init_task_list(self) -> BaseElement:
-            self.task_list = TaskManagerPage.TaskListCard.TaskList(
-                self.locate(self.task_list_xpath)
-            )
-            return self.task_list
+        def init_tasks(self) -> list:
+            task_list = self.locate(self.tasks_xpath).all()
+            for task in task_list:
+                self.tasks.append(TaskWithCheckboxAndButton(task))
+            return self.tasks
 
         @property
-        def is_task_list_initialized(self):
-            return bool(self.task_list)
-
-        class TaskList(BaseElement):
-            def __init__(self, body: Locator):
-                super().__init__(body)
-                self.task_xpath: str = ".//li"
-                self.tasks: list[BaseElement] = []
-
-            def initialize_tasks(self):
-                tasks: list[Locator] = self.locate(self.task_xpath).all()
-                for task in tasks:
-                    self.tasks.append(
-                        TaskManagerPage.TaskListCard.TaskList.TaskWithCheckbox(task)
-                    )
-                return self.tasks
-
-            @property
-            def is_populated(self):
-                return bool(self.tasks)
-
-            class TaskWithCheckbox(BaseElement):
-                def __init__(self, body: Locator):
-                    super().__init__(body)
-                    self.task_text_xpath: str = ".//span"
-                    self.checkbox_xpath: str = ".//input"
-                    self.button_xpath: str = ".//button"
-
-                    self.checkbox: BaseElement = (
-                        TaskManagerPage.TaskListCard.TaskList.TaskWithCheckbox.Checkbox(
-                            self.locate(self.checkbox_xpath)
-                        )
-                    )
-                    self.button: BaseElement = self.locate(self.button_xpath)
-
-                @property
-                def text(self):
-                    return self.locate(self.task_text_xpath).inner_text()
-
-                @property
-                def text_elem(self):
-                    return self.locate(self.task_text_xpath)
-
-                @property
-                def is_text_line_through(self):
-                    style = self.locate(self.task_text_xpath).get_attribute("style")
-                    return "line-through" in style
-
-                class Checkbox(BaseElement):
-                    def __init__(self, body: Locator):
-                        super().__init__(body)
+        def are_tasks_present(self):
+            return bool(self.tasks)
 
     class CompletedTasks(Card):
-        def __init__(self, body: Locator):
-            super().__init__(body)
+        def __init__(self, locator: Locator):
+            super().__init__(locator)
+            self.tasks_xpath: str = ".//li"
+            self.show_completed_tasks_button_xpath = ".//button"
+            self.tasks = []
+            self.show_completed_tasks_button = self.locate(
+                self.show_completed_tasks_button_xpath
+            )
+
+        def init_tasks(self) -> list:
+            task_list = self.locate(self.tasks_xpath).all()
+            for task in task_list:
+                self.tasks.append(TaskWithTextOnly(task))
+            return self.tasks
+
+        @property
+        def are_tasks_present(self):
+            return bool(self.tasks)
 
 
 class TestTaskManager:
@@ -187,15 +183,19 @@ class TestTaskManager:
         yield
         print("after the test runs")
 
-    @pytest.mark.parametrize("tip_timeout", [1000, 2000, 3000, 4000])
+    @pytest.mark.task_manager_text
+    @pytest.mark.parametrize(
+        "tip_timeout", [1000, 4000], ids=lambda t: f"timeout_{t}ms"
+    )
     def test_01_task_manager_text(self, tip_timeout: int):
         expect(self.task_manager_page.daily_tip_card.tip_text_elem).to_have_text(
             Text.STAY_FOCUSED_AND_PRIORITIZE.value, timeout=tip_timeout
         ), (
-            f"The daily tip text {Text.STAY_FOCUSED_AND_PRIORITIZE.value} "
+            f"The daily tip text '{Text.STAY_FOCUSED_AND_PRIORITIZE.value}' "
             f"did not load within {tip_timeout}ms."
         )
 
+    @pytest.mark.add_new_task
     @pytest.mark.parametrize(
         "text",
         [
@@ -208,6 +208,7 @@ class TestTaskManager:
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()",
         ],
+        ids=lambda t: f"task_{t[:10].strip()}",
     )
     def test_02_add_new_task(self, text):
         add_task_card = self.task_manager_page.add_new_task_card
